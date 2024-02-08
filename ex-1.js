8a5bc3642b4321aadbd304bebb963c50dc843b54
devsomsak <devrleang@gmail.com>
2024-02-08 17:12:08 +0700

    feat:solve problem-solving-5

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,16 @@
 function runningSum(nums) {
-    
-};
+  let runningSumResult = [];
+  let TotalSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    TotalSum += nums[i];
+    runningSumResult.push(TotalSum);
+  }
+  return runningSumResult;
+}
+
+let nums1 = [1, 2, 3, 4];
+console.log(runningSum(nums1));//[ 1, 3, 6, 10 ] 
+let nums2 = [1,1,1,1,1];
+console.log(runningSum(nums2));//[ 1, 2, 3, 4, 5 ] 
+let nums3 = [3,1,2,10,1];
+console.log(runningSum(nums3));//[ 3, 4, 6, 16, 17 ]
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,21 @@
 function moveZeroes(nums) {
-    
-};
+  let result = [];
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!nums[i] == 0) {
+      result.push(nums[i]);
+    } else {
+      count += 1;
+    }
+  }
+  for (let i = 0; i < count; i++) {
+    result.push(0);
+  }
+  return result;
+}
+
+let nums1 = [0, 1, 0, 3, 12];
+console.log(moveZeroes(nums1)); //[ 1, 3, 12, 0, 0 ]
+let nums2 = [0];
+console.log(moveZeroes(nums2)); //[ 0 ]
